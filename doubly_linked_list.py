55e369d890f9728819296e7a5ef841c03a7b8590
hamo <mohamedkholeef082@gmail.com>
2021-10-23 13:47:05 +0200

    linked-list

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -3,6 +3,7 @@ class Node:
     def __init__(self, val):
         self.val = val
         self.next = None
+        self.prev = None
 
 class MyLinkedList(object):
 
@@ -33,10 +34,14 @@ class MyLinkedList(object):
         """
         add a node at the first of the linked-list
         """
-        curr_head = self.head
         new_head = Node(val)
-        new_head.next = curr_head
-        self.head = new_head
+        if self.head is None:
+            self.head = new_head
+        else:
+            curr_head = self.head
+            curr_head.prev = new_head
+            new_head.next = curr_head
+            self.head = new_head
         self.size+=1
         
 
@@ -48,12 +53,14 @@ class MyLinkedList(object):
         curr= self.head
         if self.size == 1:
             curr.next = new_node
+            new_node.prev = curr
         elif self.size == 0:
             self.head = new_node
         else:
             for i in range((self.size)-1):
                 curr = curr.next
             curr.next = new_node
+            new_node.prev = curr
         self.size+=1
 
     def add_at_index(self, index, val):
@@ -71,8 +78,11 @@ class MyLinkedList(object):
             new_node = Node(val)
             for i in range(index):
                 if i == index-1:
-                    new_node.next = curr.next
+                    n = curr.next
+                    new_node.next = n
+                    new_node.prev = curr
                     curr.next = new_node
+                    n.prev = new_node
                 else:
                     curr = curr.next
             self.size+=1
@@ -87,32 +97,31 @@ class MyLinkedList(object):
                 head = self.head
                 if self.size > 1:
                     self.head = head.next
+                    self.head.prev = None
                     self.size-=1
                 else:
                     self.head = None
                     self.size-=1
             else:
                 curr = self.head
-                prev = None
                 for i in range(index):
-                    if i == index-1:
-                        prev = curr
-                        curr = curr.next
-                    else:
-                        curr = curr.next
+                    curr = curr.next
+                prev = curr.prev
+                    
                 if index == (self.size)-1:
                     prev.next = None
                 else:
-                    prev.next = curr.next
+                    n = curr.next
+                    prev.next = n
+                    n.prev = prev
                 self.size-=1
         
 
 
 obj = MyLinkedList()
-obj.add_at_head(23)
+obj.add_at_head(12)
 obj.add_at_tail(6)
-obj.add_at_index(1,5)
-obj.delete_at_index(0)
-
+obj.add_at_index(3,5)
+obj.delete_at_index(1)
 print(obj.get(0))
 print(obj.get(1))
